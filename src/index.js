30a0dbc66d2ce3845756d81a376d3e23f5fe9230
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-02-19 13:41:50 -0500

    Make getTask fn and taskList

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import "./style.css";
 import {TaskState} from "./taskState.js";
 
+TaskState.makeTask("MyTask", "September 16", "This is the description", 3);
+console.log(TaskState.getTaskList());
--- src/taskState.js
+++ src/taskState.js
@@ -1,25 +1,5 @@
-/*
-Task Class + Function to make new tasks
-
-taskList array (stores all tasks) - NOT EXPORTED
-getTaskList function - EXPORTED
-    return [...taskList]
-
-Task class - NOT EXPORTED
-    constructor takes name, date, priority, description
-        this.name = name, this.date = date, etc
-        this.id = crypto.randomUUID()
-        this.checked = false
-    toggleChecked function
-        ternary operator that switches checked status
-
-makeClass function
-    takes name, date, priority, description
-    task = new Task(name, date, priority, description)
-    append task to taskList
-    return task
-
-*/
+const taskList = [];
+const getTaskList = () => [...taskList];
 
 class Task {
     constructor(name, date, description, priority) {
@@ -35,8 +15,10 @@ class Task {
     }
 }
 
+const makeTask = (name, date, description, priority) => {
+    const myTask = new Task(name, date, description, priority);
+    taskList.push(myTask);
+}
 
-
-
-const TaskState = {};
+const TaskState = {getTaskList, makeTask};
 export {TaskState};
