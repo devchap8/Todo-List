2a801e703b4bce45d568f8d82e7cdaa369fdbb69
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-03-02 15:10:52 -0500

    Toggle project screen on button click

--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ import { TaskState } from "./taskState.js";
 import { DomManager } from "./domManager.js";
 import { ProjectState } from "./projectState.js";
 
+const newProjectButton = document.querySelector(".newProjectButton");
+const addProjectScreen = document.querySelector(".addProjectScreen");
+
+const addProjectButtonEventListener = () => {
+    newProjectButton.addEventListener("click", toggleAddProjectScreen);
+}
+
+const toggleAddProjectScreen = () => {
+    addProjectScreen.classList.contains("hidden") ? addProjectScreen.classList.remove("hidden") :
+        addProjectScreen.classList.add("hidden");
+}
+
 TaskState.makeTask("MyTask", "September 16", "This is the description", 1, "Project 1");
 TaskState.makeTask("MyTask", "September 16", "This is the description", 2, "Project 1");
 TaskState.makeTask("MyTask", "September 16", "This is the description", 3, "Project 1");
 TaskState.makeTask("MyTask", "September 16", "This is the description", 4, "Project 1");
 TaskState.makeTask("MyTask", "September 16", "This is the description", 5, "Project 1");
-
-for(const task of TaskState.getTaskList()) {
-    DomManager.displayTask(task);
-}
 
 ProjectState.makeProject("Project 1");
 ProjectState.makeProject("Project 2");
@@ -19,7 +27,13 @@ ProjectState.makeProject("Project 1");
 ProjectState.makeProject("Project 3");
 console.log(ProjectState.getProjectList());
 
+for(const task of TaskState.getTaskList()) {
+    DomManager.displayTask(task);
+}
 for(const project of ProjectState.getProjectList()) {
     DomManager.displayProject(project);
 }
+
+addProjectButtonEventListener();
+
 
