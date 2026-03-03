7ddbfbac60fd7e1ca705c7c2795af9eba8f44369
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-03-03 12:22:08 -0500

    Make button to show all tasks

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import { TaskState } from "./taskState.js";
 import { DomManager } from "./domManager.js";
 import { ProjectState } from "./projectState.js";
 import { FormHandling } from "./formHandling.js";
+import { TaskSorting } from "./taskSorting.js";
 
 const addTaskButton = document.querySelector(".addTaskButton");
 const submitTaskButton = document.querySelector(".submitTaskButton")
 const newProjectButton = document.querySelector(".newProjectButton");
 const projectForm = document.querySelector(".addProjectScreen form");
+const allTasksButton = document.querySelector(".sidebarAll");
 
 
 const addTaskButtonEventListener = () => {
@@ -22,6 +24,10 @@ const addProjectButtonEventListener = () => {
 }
 function addProjectFormSubmitEventListener() {
     projectForm.addEventListener("submit", FormHandling.parseProjectFormData);
+}
+
+const addAllTasksButtonEventListener = () => {
+    allTasksButton.addEventListener("click", TaskSorting.displayTasksAll);
 }
 
 
@@ -49,4 +55,5 @@ addProjectButtonEventListener();
 addProjectFormSubmitEventListener();
 addTaskButtonEventListener();
 addSubmitTaskButtonEventListener();
+addAllTasksButtonEventListener();
 
--- src/taskSorting.js
+++ src/taskSorting.js
@@ -1,1 +1,17 @@
+import { TaskState } from "./taskState.js";
+import { DomManager } from "./domManager.js";
+
 const taskDisplay = document.querySelector(".taskDisplay");
+
+const clearTaskDisplay = () => {
+    taskDisplay.innerHTML = "";
+}
+
+const displayTasksAll = () => {
+    clearTaskDisplay();
+    const taskList = TaskState.getTaskList();
+    taskList.forEach(task => DomManager.displayTask(task));
+}
+
+const TaskSorting = {clearTaskDisplay, displayTasksAll};
+export {TaskSorting};
